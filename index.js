e4ab91a3510b2a3599c9342881196a452039c7e4
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-17 19:31:11 +0600

    add onclick on the button and change background color

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ chack.style.textAlign = 'center';
 chack.style.color = 'red';
 
 
+function makeRead() {
+    document.body.style.backgroundColor = 'red';
+}
+
+const blueButton = document.getElementById('make-blue');
+blueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}

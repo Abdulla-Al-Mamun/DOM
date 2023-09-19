49a3a1586602879864df60a73ba06615a913454b
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-19 20:14:03 +0600

    added new list in list container

--- delagate/delegate.js
+++ delagate/delegate.js
@@ -4,3 +4,15 @@ document.getElementById('list-container').addEventListener('click', function (ev
 
 })
 
+document.getElementById('btn-id').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+
+    const li = document.createElement('li');
+    li.innerText = 'added new item';
+
+    li.classList.add('item');
+    listContainer.appendChild(li);
+
+
+})
